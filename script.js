d1459c27e3fe1808e83db6a2a0bd5bbea82e49b9
brad-a11y <brad@oneteam.services>
2025-08-07 15:38:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
     async function submitToGoogleForm(serialNumber) {
         const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfqcU9LE0cvOXwF1lN_Ge-lLiAzQkC-KdsiTCafc7I4fhZajg/formResponse";
         const dateEntry = "entry.1741188331";
-        const serialNumberEntry = "entry.839396458";
+        const serialNumberEntry = "YPqjbf";
 
         const now = new Date();
         const formData = new FormData();
